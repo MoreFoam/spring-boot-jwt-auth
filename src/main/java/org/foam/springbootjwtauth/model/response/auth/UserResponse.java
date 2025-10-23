7b7fbab32e5b8c354dbf2bad026892675e256d4c
MoreFoam <chris.marler@me.com>
2025-10-22 23:06:38 -0500

    Remove unnecessary fields from UserResponse DTO

--- src/main/java/org/foam/springbootjwtauth/model/response/auth/UserResponse.java
+++ src/main/java/org/foam/springbootjwtauth/model/response/auth/UserResponse.java
@@ -11,9 +11,4 @@ public class UserResponse {
     private Long id;
     private String username;
     private String email;
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String phoneNumber;
-    private String profilePhotoUrl;
 }
