8719f03effd04ddf8563461358691d30ff278878
MoreFoam <chris.marler@me.com>
2025-10-31 18:42:26 -0500

    Fix 'DaoAuthenticationProvider()' is deprecated warning. Spring Security 6 requires that the UserDetailsService be included in the constructor.

--- src/main/java/org/foam/springbootjwtauth/config/SecurityConfig.java
+++ src/main/java/org/foam/springbootjwtauth/config/SecurityConfig.java
@@ -73,8 +73,7 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(
             UserDetailsService userDetailsService,
             PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(authenticationProvider);
